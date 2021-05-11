5a28535906f6aa9db819882070cac932c5811e16
shim99887 <shim99887@naver.com>
2021-05-11 23:56:03 +0900

    init

---
+++ src/main/java/com/temp/tempproject/TempProjectApplication.java
@@ -0,0 +1,13 @@
+package com.temp.tempproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TempProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/temp/tempproject/TempProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.temp.tempproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
