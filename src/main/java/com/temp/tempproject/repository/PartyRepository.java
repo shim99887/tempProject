bb336083aa24b419a34033133d5e4dda922dd0d0
shim99887 <shim99887@naver.com>
2021-05-12 00:18:22 +0900

    init

--- src/main/java/com/temp/tempproject/repository/PartyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.temp.tempproject.repository;
-
-import com.temp.tempproject.entity.Party;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface PartyRepository extends JpaRepository<Party, Integer>{
-
-}
--- src/main/java/com/temp/tempproject/repository/PlayerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.temp.tempproject.repository;
-
-import com.temp.tempproject.entity.PartyDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlayerRepository extends JpaRepository<PartyDetail, Integer> {
-}
